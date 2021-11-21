69881e19e1bfc313bf88ebdc8dca4f0391580cd0
EC2 Default User <ec2-user@ip-172-31-25-35.ec2.internal>
2021-11-21 12:01:02 +0000

    test5

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,24 @@
 class BooksController < ApplicationController
+
   def new
-    @books = books.new
-  end
+    @book = book.new
   
   def create
-    @books = books.new(post_image_params)
-    @books.user_id = current_user.id
-    @books.save
-    redirect_to post_images_path
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book.id), notice: "successfully created book!"
+    else
+    @books = Book.all
+    render :index
   end
 
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+     @book = Book.find(params[:id])
   end
 
   def destroy
